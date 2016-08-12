f18a917563ffb914a900723bba1b6be238b9c435
Joe Karasek <joseph.karasek@gmail.com>
2016-08-12 12:35:04 -0700

    add content and basic funciton of patient review section

--- html/js/main.js
+++ html/js/main.js
@@ -23,6 +23,14 @@ $(document).ready(function() {
     slidesToShow: 4,
     slidesToScroll: 4
   });
+  $('.reviews__slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 4000
+  });
   $('.ui-accordion-header').click(function () {
     $(this).parent().find('.ui-accordion-content').addClass('folded');
     $(this).next().removeClass('folded');
