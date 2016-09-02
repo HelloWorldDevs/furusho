b5a98187eb8a3cc35ebb72a2f39bebf67b5f047f
Joe Karasek <joseph.karasek@gmail.com>
2016-09-02 11:45:00 -0700

    clean up styling around the next and prev buttons

--- html/_site/js/main.js
+++ html/_site/js/main.js
@@ -57,14 +57,21 @@ $(document).ready(function() {
     nextArrow: "<button type='button' class='slick-next team-slick-nav'>Next</button>",
     responsive: [
       {
-        breakpoint: 760,
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 990,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 550,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
